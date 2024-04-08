bc60949d4ab02e0006b3d6e903a552f94ac2fa40
Ulises Lárraga <uliseslarraga@gmail.com>
2024-04-07 22:49:22 -0600

    feat(iam): create s3 read only policy

--- modules/compute/iam.tf
+++ modules/compute/iam.tf
@@ -15,6 +15,30 @@ data "aws_iam_policy_document" "ssm_policy" {
     ]
     resources = ["*"]
   }
+} 
+
+resource "aws_iam_policy" "s3_read_only_policy" {
+  name        = "s3_readonly_policy_${var.environment}"
+  description = "Allows readonly access to s3 bucket objects"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:Get*",
+          "s3:List*",
+          "s3:Describe*",
+          "s3-object-lambda:Get*",
+          "s3-object-lambda:List*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -37,6 +61,11 @@ resource "aws_iam_role" "role" {
     name   = "policy-${var.environment}-8675309"
     policy = data.aws_iam_policy_document.ssm_policy.json
   }
+}
+
+resource "aws_iam_role_policy_attachment" "s3-read-only-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.s3_read_only_policy.arn
 }
 
 resource "aws_ssm_document" "ssm_document" {
